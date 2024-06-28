b836bdd76b44dabbe9deb28c1c2da527ac6f7169
mandis-ncs <amnd.castro@gmail.com>
2024-06-28 15:22:13 -0300

    feat: updating user

--- login-auth-api/src/main/java/com/example/login_auth_api/domain/user/User.java
+++ login-auth-api/src/main/java/com/example/login_auth_api/domain/user/User.java
@@ -1,10 +1,7 @@
 package com.example.login_auth_api.domain.user;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @Table(name = "users")
@@ -12,6 +9,7 @@ import lombok.Setter;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class User {
 
     @Id
